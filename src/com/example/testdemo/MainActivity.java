59ffa759c6164447c0aa01f1aac162e37782bb13
LuYuan <yuan.lu@i-soft.com.cn>
2013-02-18 16:37:38 +0800

    SMS ready!

--- src/com/example/testdemo/MainActivity.java
+++ src/com/example/testdemo/MainActivity.java
@@ -4,14 +4,51 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
+import android.view.View;
+import android.content.Intent;
+import android.telephony.SmsManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.app.PendingIntent;
+import android.util.Log;
+
 public class MainActivity extends Activity {
+	
+	private Button BtnSendMsg;
+	private EditText EdtMsgContent;
+	private EditText EdtMsgNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        BtnSendMsg = (Button) findViewById(R.id.Send);
+        EdtMsgContent = (EditText) findViewById(R.id.editText3);
+        EdtMsgNumber = (EditText) findViewById(R.id.editText2);
+       
+        BtnSendMsg.setOnClickListener(new Button.OnClickListener() {
+        	public void onClick(View arg0) {
+        		String Msg = EdtMsgContent.getText().toString();
+        		String Number = EdtMsgNumber.getText().toString();
+        		smsSender(Number, Msg);
+        	}
+        });
+        
+        
+        
     }
 
+    void smsSender(String phone, String message) {
+    	SmsManager sms = SmsManager.getDefault();
+    	PendingIntent pi = PendingIntent.getActivity(this, 0, new Intent(this, MainActivity.class), 0);
+    	try {
+    		sms.sendTextMessage(phone, null, message, pi, null);
+    	}catch(Exception e){
+    		Log.e("SmsSending","SendException", e );
+    	}
+
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
