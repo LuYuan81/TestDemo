13fcf0833255542abd79d3634e23957d34cb1c97
LuYuan <yuan.lu@i-soft.com.cn>
2013-03-15 20:59:06 +0800

    Add 1 activity

---
+++ src/com/example/testdemo/TestPhoneBook.java
@@ -0,0 +1,16 @@
+package com.example.testdemo;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+
+public class TestPhoneBook extends Activity{
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_testphonebook);
+	}
+}
+
+
